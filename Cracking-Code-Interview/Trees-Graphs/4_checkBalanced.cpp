f38e13759281aa8e36c2c92229557478a71e2309
Moses Karemera <ndagijim22@up.edu>
2023-07-14 18:33:29 -0700

    Added questions files and their description

---
+++ Cracking-Code-Interview/Trees-Graphs/4_checkBalanced.cpp
@@ -0,0 +1,17 @@
+/*
+   Check Balanced: Implement a function to check if a binary tree is balanced. For the purposes of 
+    this question, a balanced tree is defined to be a tree such that the heights
+    of the two subtrees of any node never differ by more than one.
+
+        ALGORITHM
+    - Use the fact that two subtrees are balanced if their heights never differ by more than 1
+    - recurse thru the entire tree, and for each node, compute height of its subtree
+*/
+
+int getHeight(TreeNode root){
+    //base case
+    if (root == NULL)
+        return -1;
+    return 
+    
+}
